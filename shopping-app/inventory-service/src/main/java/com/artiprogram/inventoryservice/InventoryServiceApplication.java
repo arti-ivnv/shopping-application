6361efe537144c8388d12b107c18da26260f2568
Artem Ivanov <arti.ivnv@gmail.com>
2023-07-07 19:32:00 +0200

    service discovery with Eureka (Server & Client)

--- shopping-app/inventory-service/src/main/java/com/artiprogram/inventoryservice/InventoryServiceApplication.java
+++ shopping-app/inventory-service/src/main/java/com/artiprogram/inventoryservice/InventoryServiceApplication.java
@@ -3,12 +3,14 @@ package com.artiprogram.inventoryservice;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.artiprogram.inventoryservice.model.Inventory;
 import com.artiprogram.inventoryservice.repository.InventoryRepository;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class InventoryServiceApplication {
 
 	public static void main(String[] args) {
