0e24e37ea1e7030577d167841d4926f25bb028c9
Artem Ivanov <arti.ivnv@gmail.com>
2023-07-08 23:39:04 +0200

    taking out separate services

--- services_separately/order-service/src/main/java/com/artiprogram/orderservice/repository/OrderRepository.java
+++
@@ -1,9 +0,0 @@
-package com.artiprogram.orderservice.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.artiprogram.orderservice.model.Order;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-    
-}
--- services_separately/product-service/src/main/java/com/artiprogram/productservice/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.artiprogram.productservice.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.artiprogram.productservice.model.Product;
-
-public interface ProductRepository extends MongoRepository<Product, String>{
-    
-}
